3a4107183169422b9cb88fc1da20e61aaa199568
Michael Alexander <beefsack@gmail.com>
2012-11-22 13:45:01 +0800

    Working migrations.

--- script/db/migrate.js
+++ script/db/migrate.js
@@ -1,3 +1,10 @@
 var migrate = require('../../lib/db/migrate');
 
-migrate.migrate();
+migrate.migrate()
+.then(function() {
+  console.log('Migrations run successfully');
+  process.exit(0);
+}, function(error) {
+  console.log(error.stack);
+  process.exit(1);
+});
